eb0719a9a9333ee33537104263751eab76c8014b
Thomas Rowntree <thomasr@canva.com>
2025-02-06 23:08:01 +1030

    Add practice endpoints

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
+users = {}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/play/{game_id}")
+async def game(
+    game_id: str,
+    request: Request
+    ):
+
+    user_id = request.cookies.get("user_id", None)
+    if user_id not in users:
+        user_id = str(len(users))
+        users[user_id] = dict(
+            name="New User",
+        )
+
+    response = Response(
+        content=str({"game_id": game_id, "user_id": user_id, "user": users[user_id]}),
+    )
+    response.set_cookie(key="user_id", value=user_id)
+
+    return response
+
+@app.get("/update_user")
+async def update_user(key:str, value:str, request: Request):
+    user_id = request.cookies.get("user_id", None)
+    if user_id is None:
+        return {"message": "User not found"}
+
+    users[user_id][key] = value
+
+    return {"message": "User updated", "user": users[user_id]}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
