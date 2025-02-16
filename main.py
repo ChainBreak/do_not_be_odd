697229abe71bab31b324fa8afef8df852e7fc8cf
Thomas Rowntree <thomasr@canva.com>
2025-02-16 22:44:26 +1030

    Add dependancy injections

--- main.py
+++ main.py
@@ -1,44 +1,64 @@
 from typing import Optional
-
-from fastapi import FastAPI, Request, Response
+import uuid
+from collections import defaultdict
+import dataclasses
+from fastapi import FastAPI, Request, Response, Depends
+from loguru import logger
 
 app = FastAPI()
 
-users = {}
+@dataclasses.dataclass
+
+class User():
+    id: str
+    name: str
+
+    def create_new_user():
+        return User(
+            id=str( uuid.uuid4() ),
+            name="New User",
+        )
+
+users = defaultdict(User.create_new_user)
+
+
+def get_user(request: Request, response: Response):
+    """Dependency that adds a unqiue user_id to the cookie in the response if it does not exist"""
+
+    user_id = request.cookies.get("user_id", None)
+
+    user = users[user_id]
+
+    response.set_cookie(key="user_id", value=user.id)
+
+    return user
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/play/{game_id}")
 async def game(
     game_id: str,
-    request: Request
+    user: str = Depends(get_user),
     ):
 
-    user_id = request.cookies.get("user_id", None)
-    if user_id not in users:
-        user_id = str(len(users))
-        users[user_id] = dict(
-            name="New User",
-        )
+    logger.debug(users)
+    return {"game_id": game_id, "user": user}
 
-    response = Response(
-        content=str({"game_id": game_id, "user_id": user_id, "user": users[user_id]}),
-    )
-    response.set_cookie(key="user_id", value=user_id)
-
-    return response
 
 @app.get("/update_user")
-async def update_user(key:str, value:str, request: Request):
-    user_id = request.cookies.get("user_id", None)
-    if user_id is None:
-        return {"message": "User not found"}
+async def update_user(
+    key:str,
+    value:str,
+    user: str = Depends(get_user),
+    ):
 
-    users[user_id][key] = value
+    user.__setattr__(key, value)
 
-    return {"message": "User updated", "user": users[user_id]}
+    return {"message": "User updated", "user": user}
 
 
 @app.get("/items/{item_id}")
