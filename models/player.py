dff7086a61e02f7d5991237b78b6af9d24126aa2
Thomas Rowntree <thomasr@canva.com>
2025-03-03 23:29:53 +1030

    Make vue work and display player list

--- models/player.py
+++ models/player.py
@@ -1,9 +1,11 @@
 import dataclasses
 from models import game
-
+import codename
+import uuid
 @dataclasses.dataclass
 class Player():
     session_id: str
     game: "game.Game"
-    name: str = "Player"
+    id: str = dataclasses.field(default_factory=lambda: str(uuid.uuid4()))
+    name: str = dataclasses.field(default_factory=codename.codename)
     playing: bool = False
--- schemas/game_schema.py
+++ schemas/game_schema.py
@@ -3,20 +3,25 @@ from models import game, player
 
 class GameSchema(BaseModel):
     game_id: str
+    player_name: str
     num_players: int
     player_list: list["PlayerSchema"]
 
     def __init__(self,game: game.Game, player: player.Player):
         super().__init__(
             game_id = game.id,
+            player_name = player.name,
             num_players = len(game.players),
             player_list = [PlayerSchema(player) for player in game.players.values()],
         )
 
 class PlayerSchema(BaseModel):
     name: str
+    id: str
 
     def __init__(self,player: player.Player):
         super().__init__(
-            name=player.name)
+            name=player.name,
+            id=player.id,
+        )
 
