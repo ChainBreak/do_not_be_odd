7a22b53567a0dde8f69c767f8fc2b0e09bc5cccc
Thomas Rowntree <thomasr@canva.com>
2025-03-13 21:43:36 +1030

    Give each event its own endpoint

--- routes/operations.py
+++ routes/operations.py
@@ -16,54 +16,8 @@ router = APIRouter()
 def new_game(
     model: model.Model = Depends(deps.model_dependency),
     ):
-    
     game = model.create_new_game()
-
     return RedirectResponse(url=f"/game/{game.id}")
-
-@router.get("/update_user")
-async def update_user(
-    key:str,
-    value:str,
-    user: str = Depends(deps.session_dependency),
-    ):
-
-    user.__setattr__(key, value)
-
-    return {"message": "User updated", "user": user}
-
-
-@router.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-class EventSchema(BaseModel):
-    id: str
-    payload: dict
-
-@router.post("/game/{game_id}/event")
-async def game_event(
-    event: EventSchema,
-    player: player.Player = Depends(deps.player_dependency),
-    game: game.Game = Depends(deps.game_dependency),
-    ):
-
-    match event.id:
-
-        case "update_player_name":
-            player.name = event.payload["name"]
-            return {"message": "Player name updated"}
-        
-        case "join_round":
-            player.playing = True
-            logger.info(f"Player Joined {player.playing}")
-            return {"message": "Player joined round"}
-        
-        case "spectate_round":
-            player.playing = False
-            logger.info(f"Player Joined {player.playing}")
-            return {"message": "Player left round"}
 
 
 @router.get("/game/{game_id}/state")
@@ -71,7 +25,29 @@ async def get_game_state(
     player: player.Player = Depends(deps.player_dependency),
     game: game.Game = Depends(deps.game_dependency),
     ) -> game_schema.GameSchema:
-
     game.update()
-
     return game_schema.GameSchema(game=game, player=player)
+
+
+@router.post("/game/{game_id}/join_round")
+async def join_round(
+    player: player.Player = Depends(deps.player_dependency),
+    ):
+    player.playing = True
+
+@router.post("/game/{game_id}/spectate_round")
+async def spectate_round(
+    player: player.Player = Depends(deps.player_dependency),
+    ):
+    player.playing = False
+
+
+class UpdatePlayerNameSchema(BaseModel):
+    name: str
+
+@router.post("/game/{game_id}/update_player_name")
+async def update_player_name(
+    payload : UpdatePlayerNameSchema,
+    player: player.Player = Depends(deps.player_dependency),
+    ):
+    player.name = payload.name
